b90a1bad93024067647c3459340e9ea7b2d227ee
Dimitrios Zisis <zdimitris@outlook.com>
2019-04-18 18:17:27 +0300

    1st commit

---
+++ client.py
@@ -0,0 +1,57 @@
+import socket
+from abc import ABCMeta, abstractmethod
+
+
+class Client:
+
+    def __init__(self, host, port, protocol):
+        self.protocol = protocol
+        self.host = host
+        self.port = port
+        self.data_socket = socket.socket()
+        self.data_socket.connect((self.host, self.port))
+
+    def __init__(self, port, protocol):
+        self.protocol = protocol
+        self.host = 'localhost'
+        self.port = port
+        self.data_socket = socket.socket()
+        self.data_socket.connect((self.host, self.port))
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.host = 'localhost'
+        self.port = 1234
+        self.data_socket = socket.socket()
+        self.data_socket.connect((self.host, self.port))
+
+    def open(self):
+
+        # Prepare request, according to client's protocol
+        out_msg = self.protocol.prepare_request()
+
+        # Send request encoded
+        self.data_socket.send(out_msg.encode())
+
+        # Receive message from server
+        input_msg = self.data_socket.recv(1024)
+
+        # Get server's message in right format
+        input_msg = input_msg.decode('UTF-8')  # convert to string
+        input_msg = input_msg.replace('\n', '')  # remove newline character
+
+        # Process reply, according to client's protocol
+        self.protocol.process_reply(input_msg)
+
+        # Close the connection with server
+        self.data_socket.close()
+
+
+class ClientProtocol:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def prepare_request(self): raise NotImplementedError
+
+    @abstractmethod
+    def process_reply(self, input_msg): raise NotImplementedError
---
+++ server.py
@@ -0,0 +1,158 @@
+import socket
+import threading
+from abc import ABCMeta, abstractmethod
+
+
+class IterativeServer:
+
+    def __init__(self, port, protocol):
+        self.protocol = protocol
+        self.port = port
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.port = 1234    # default port 1234
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def listen(self):
+
+        # Put the socket into listening mode
+        self.connection_socket.listen(5)
+        print('Server is listening to port ', self.port)
+
+        # A forever loop until we interrupt it
+        while True:
+
+            try:
+                # Establish connection with client.
+                conn, address = self.connection_socket.accept()
+                print('Received request from ', address)
+
+                # Get client's message
+                input_msg = conn.recv(1024)
+
+                # Get client's message in right format
+                input_msg = input_msg.decode('UTF-8')  # convert to string
+                input_msg = input_msg.replace('\n', '')  # remove newline character
+
+                # Protocol, in order to generate the output
+                out_msg = self.protocol.process_request(input_msg)
+
+                # Send output to client
+                conn.send(out_msg.encode())
+
+                # Close the connection with the client
+                conn.close()
+            except:
+                print('Client disconnected')
+                exit(1)
+
+
+class Server:
+
+    def __init__(self, port, protocol):
+        self.protocol = protocol
+        self.port = port
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.port = 1234    # default port 1234
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def listen(self):
+
+        # Put the socket into listening mode
+        self.connection_socket.listen(5)
+        print('Server is listening to port ', self.port)
+
+
+        try:
+
+            # Establish connection with client
+            conn, address = self.connection_socket.accept()
+            print('Received request from ', address)
+
+            # Get client's message
+            input_msg = conn.recv(1024)
+
+            # Get client's message in right format
+            input_msg = input_msg.decode('UTF-8')  # convert to string
+            input_msg = input_msg.replace('\n', '')  # remove newline character
+
+            # Protocol, in order to generate the output
+            out_msg = self.protocol.process_request(input_msg)
+
+            # Send output to client
+            conn.send(out_msg.encode())
+
+            # Close the connection with the client
+            conn.close()
+        except:
+            print('Client disconnected')
+            exit(1)
+
+
+class MultiThreadedServer:
+
+    def __init__(self, port, protocol):
+        self.protocol = protocol
+        self.port = port
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.port = 1234    # default port 1234
+        self.connection_socket = socket.socket()
+        self.connection_socket.bind(('', self.port))    # localhost
+
+    def listen(self):
+
+        # Put the socket into listening mode
+        self.connection_socket.listen(5)
+        print('Server is listening to port ', self.port)
+
+        while True:
+
+            # Establish connection with client
+            conn, address = self.connection_socket.accept()
+            print('Received request from ', address)
+
+            # Start thread (each connection == new thread)
+            threading.Thread(target=self.server_thread, args=(conn, address)).start()
+
+    def server_thread(self, conn, address):
+        while True:
+            try:
+
+                # Get client's message
+                input_msg = conn.recv(1024)
+
+                # Get client's message in right format
+                input_msg = input_msg.decode('UTF-8')  # convert to string
+                input_msg = input_msg.replace('\n', '')  # remove newline character
+
+                # Protocol, in order to generate the output
+                out_msg = self.protocol.process_request(input_msg)
+
+                # Send output to client
+                conn.send(out_msg.encode())
+
+                # Close the connection with the client
+                conn.close()
+            except:
+                print('Client disconnected')
+                exit(1)
+
+
+class ServerProtocol:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def process_request(self, input_msg): raise NotImplementedError
